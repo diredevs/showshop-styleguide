ba59089dd35226dcd3cfb37af14b08df0d00c5c1
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-05-12 23:49:04 -0300

    Better product details toggle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,7 @@
 $('document').ready(function () {
   $('.card-display').on('click', function (event) {
     event.preventDefault();
-    $(this).toggleClass('active');
-    var target = $(this).data('productDetail');
-    target = '#' + target;
-    console.log(target);
-    $(target).slideToggle();
+    $('#' + $(this).toggleClass('active').data('productDetail')).stop().slideToggle();
+    event.stopPropagation();
   });
 });
