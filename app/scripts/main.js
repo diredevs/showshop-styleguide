eff8a53904c453f63ace15a10b34539db2dc951e
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-05-12 23:32:44 -0300

    Add an initial state for a product's details slide down

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('document').ready(function () {
+  $('.card-display').on('click', function (event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    var target = $(this).data('productDetail');
+    target = '#' + target;
+    console.log(target);
+    $(target).slideToggle();
+  });
+});
