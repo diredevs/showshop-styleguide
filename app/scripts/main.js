5646b099886baf364a3e8fc6030be599e7349b83
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-05-19 16:39:18 -0300

    Add slide menu for cart

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,15 @@
 'use strict';
 
 $('document').ready(function () {
+
   $('.card-display').on('click', function (event) {
     event.preventDefault();
     $('#' + $(this).toggleClass('active').data('productDetail')).stop().slideToggle();
     event.stopPropagation();
+  });
+
+  $('.toggle-cart').on('click', function (event) {
+    event.preventDefault();
+    $('#cart').toggle();
   });
 });
