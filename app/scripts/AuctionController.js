f02a14d855d588776358573559af2b5b75828c4b
luizrogeriocn <luizrogeriocn@gmail.com>
2014-06-05 00:13:34 -0300

    Add auction price and currency

--- app/scripts/AuctionController.js
+++ app/scripts/AuctionController.js
@@ -1,10 +1,10 @@
 angular.module("showshop").controller("AuctionController", ['$scope', function(scope){
 
 	scope.auctions = [
-						{id: '1', product_name: 'Playstation 4', time_left: new Date(0, 0, 0, 0, 57, 10, 0), counter: '10', timing_out: false}, 
-						{id: '2', product_name: 'Xbox One', time_left: new Date(0, 0, 0, 0, 8, 0, 0), counter: '15', timing_out: true}, 
-						{id: '3', product_name: 'Notebook Gamer CCE', time_left: new Date(0, 0, 0, 0, 38, 15, 0), counter: '154', timing_out: false},
-						{id: '4', product_name: 'Monitor LG Ultra Wide IPS', time_left: new Date(0, 0, 0, 0, 9, 9, 0), counter: '7', timing_out: true}
+						{id: '1', product_name: 'Playstation 4', price: '3900', time_left: new Date(0, 0, 0, 0, 57, 10, 0), counter: '10', timing_out: false}, 
+						{id: '2', product_name: 'Xbox One', price: '1800', time_left: new Date(0, 0, 0, 0, 8, 0, 0), counter: '15', timing_out: true}, 
+						{id: '3', product_name: 'Notebook Gamer CCE', price: '1200', time_left: new Date(0, 0, 0, 0, 38, 15, 0), counter: '154', timing_out: false},
+						{id: '4', product_name: 'Monitor LG Ultra Wide IPS', price: '1600', time_left: new Date(0, 0, 0, 0, 9, 9, 0), counter: '7', timing_out: true}
 						];
 
 	scope.update_timer = function(){
