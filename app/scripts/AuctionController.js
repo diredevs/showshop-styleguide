31779b19fe95410eda78172d8460acbbcf694bf4
luizrogeriocn <luizrogeriocn@gmail.com>
2014-06-04 23:57:28 -0300

    Updates countdown timers. closes #6

--- app/scripts/AuctionController.js
+++ app/scripts/AuctionController.js
@@ -1,10 +1,10 @@
 angular.module("showshop").controller("AuctionController", ['$scope', function(scope){
 
 	scope.auctions = [
-						{id: '1', product_name: 'Playstation 4', time_left: new Date(0, 0, 0, 8, 57, 10, 0), counter: '10'}, 
-						{id: '2', product_name: 'Xbox One', time_left: new Date(0, 0, 0, 12, 6, 0, 0), counter: '15'}, 
-						{id: '3', product_name: 'Notebook Gamer CCE', time_left: new Date(0, 0, 0, 1, 5, 15, 0), counter: '154'},
-						{id: '4', product_name: 'Monitor LG Ultra Wide IPS', time_left: new Date(0, 0, 0, 1, 10, 9, 0), counter: '7'}
+						{id: '1', product_name: 'Playstation 4', time_left: new Date(0, 0, 0, 0, 57, 10, 0), counter: '10', timing_out: false}, 
+						{id: '2', product_name: 'Xbox One', time_left: new Date(0, 0, 0, 0, 6, 0, 0), counter: '15', timing_out: false}, 
+						{id: '3', product_name: 'Notebook Gamer CCE', time_left: new Date(0, 0, 0, 0, 2, 15, 0), counter: '154', timing_out: false},
+						{id: '4', product_name: 'Monitor LG Ultra Wide IPS', time_left: new Date(0, 0, 0, 0, 10, 9, 0), counter: '7', timing_out: false}
 						];
 
 	scope.update_timer = function(){
