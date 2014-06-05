8c8edca6be3c2b9610ede68cf905cfb1b90e9f5a
luizrogeriocn <luizrogeriocn@gmail.com>
2014-06-04 22:39:55 -0300

    Add more stuff to auction controller/view

--- app/scripts/AuctionController.js
+++ app/scripts/AuctionController.js
@@ -1,10 +1,10 @@
 angular.module("showshop").controller("AuctionController", ['$scope', function(scope){
 
 	scope.auctions = [
-						{id: '1', product_name: 'playstation 4'}, 
-						{id: '2', product_name: 'Xbox One'}, 
-						{id: '3', product_name: 'Notebook Gamer CCE - Linux'},
-						{id: '4', product_name: 'Monitor LG Ultra Wide IPS'}
+						{id: '1', product_name: 'Playstation 4', time_left: new Date(0, 0, 1, 23, 57, 10, 0), counter: '10'}, 
+						{id: '2', product_name: 'Xbox One', time_left: new Date(0, 0, 1, 12, 23, 0, 0), counter: '15'}, 
+						{id: '3', product_name: 'Notebook Gamer CCE', time_left: new Date(0, 0, 0, 0, 5, 15, 0), counter: '154'},
+						{id: '4', product_name: 'Monitor LG Ultra Wide IPS', time_left: new Date(0, 0, 0, 23, 10, 9, 0), counter: '7'}
 						];
 
 }]);
